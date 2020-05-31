e97bb10fbe9a30b2724f83bf05c1904e7d2cc692
lamalamaMark <mark@lamalama.nl>
2020-05-31 12:02:04 +0200

    add service provider

---
+++ src/Wishlist.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LamaLama\Wishlist;
+
+class Wishlist
+{
+
+}
---
+++ src/WishlistServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LamaLama\Wishlist
+
+use Illuminate\Support\ServiceProvider;
+use LamaLama\Wishlist\Wishlist;
+
+class WishlistServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        $this->app->bind('wishlist', function () {
+            return new Wishlist();
+        });
+    }
+}
